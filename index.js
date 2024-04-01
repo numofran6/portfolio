ba3b1db2e3a91032b5410cbd7f3a1e936ecca29b
Numo Francis <numofran6@gmail.com>
2024-04-01 17:44:15 +0000

    Pop-up link functionality

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
 	const toggleBtn = document.querySelector("#toggle-popup")
 	const pageNav = document.querySelector('#page-nav')
 	const popup = document.querySelector('.pop-up')
+	const popupLinks = popup.querySelectorAll('a')
 
 	toggleBtn.addEventListener('click', (e) => {
 		e.preventDefault();
 
 		popup.classList.toggle('visible')
 		pageNav.classList.toggle('visible')
+	})
+
+	popupLinks.forEach(link => {
+		link.addEventListener('click', (e) => {
+			popup.classList.toggle('visible')
+			pageNav.classList.toggle('visible')
+		})
 	})
 })
